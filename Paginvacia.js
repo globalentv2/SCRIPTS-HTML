2f0b06a46bce8ccfb18f94c35aa862fd8147dbea
globalentv2 <globalentv2@gmail.com>
2019-06-27 14:13:06 -0300

    Update Paginvacia.js

--- Paginvacia.js
+++ Paginvacia.js
@@ -1,17 +1,3 @@
-<SCRIPT LANGUAGE='JavaScript'>
-<!-- Hide from JavaScript-Impaired Browsers
- 	document.write('<HTML>
-<NOFRAMES>
-<HEAD>
-<style type'
- +'="text/css">.replbq{width:100%}</style>
-
-</HEAD>
-<'
- +'BODY>
-
-<BODY>
-</NOFRAMES>');
+document.write('<HTML><NOFRAMES><HEAD><style type'
++'="text/css">.replbq{width:100%}</style></HEAD><'+'BODY><BODY></NOFRAMES>');
 // End Hiding -->
-</SCRIPT>
-<!-- Size: 245 bytes --> 
