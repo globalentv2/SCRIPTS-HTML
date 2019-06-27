ea7d8b469b5603289be37f26e327119f31057ed9
globalentv2 <globalentv2@gmail.com>
2019-06-27 14:24:59 -0300

    Create paginaLEIDA.js

---
+++ paginaLEIDA.js
@@ -0,0 +1,166 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!-- Hide from JavaScript-Impaired Browsers
+ 	document.write("<!DOCTYPE html>
+<html lang='en'>
+<he"
+ +"ad>
+  
+  <meta charset='UTF-8'>
+ 
+<style>
+@import "
+ +"url(http://fonts.googleapis.com/css?family=Open+Sa"
+ +"ns);
+ 
+
+html,body{
+  color:white;
+  font-family:'O"
+ +"pens Sans',helvetica;  
+  height:100%;
+  width:100"
+ +"%;
+  margin: 0px;
+}
+ 
+.portada{
+   background: url"
+ +"(http://jorgemiana.latinowebs.com/ANDROID-APK/DIVC"
+ +"SS/FONDOsobrio.png) no-repeat fixed center;
+   -we"
+ +"bkit-background-size: cover;
+   -moz-background-si"
+ +"ze: cover;
+   -o-background-size: cover;
+   backgr"
+ +"ound-size: cover;
+   height: 100%;
+   width: 100% "
+ +";
+   text-align: center;
+ 
+ }
+.Fondo { background:"
+ +" url(http://jorgemiana.latinowebs.com/ANDROID-APK/"
+ +"DIVCSS/FONDO-TD.png);
+-webkit-border-radius: 28;
+-"
+ +"moz-border-radius: 28;
+border-radius: 28px;
+text-s"
+ +"hadow: 3px 2px 1px #9daef5;
+-webkit-box-shadow: 6p"
+ +"x 5px 24px #666666;
+-moz-box-shadow: 6px 5px 24px "
+ +"#666666;
+box-shadow: 6px 5px 24px #666666;
+font-fa"
+ +"mily: Arial;
+  color: #fafafa;
+  font-size:27px;
+ "
+ +" padding: 19px;
+  text-decoration: none;
+height: 1"
+ +"00%;
+width: 100%;
+text-align: center;
+border:outse"
+ +"t gold;
+font-size:0.8em;
+padding:8px;
+ }
+button {
+"
+ +"	border: 10px gold;
+	background-color:c6ddba;
+	col"
+ +"or: #f2f2f2;
+	padding: 5px;
+	font-size: 36px;font-"
+ +"weight:bold;
+	border-radius: 5px;
+	position: relat"
+ +"ive;
+	box-sizing: border-box;
+	transition: all 500"
+ +"ms ease; 
+}
+button:before {
+	content:'';
+	position"
+ +": absolute;
+	top: 0px;
+	left: 0px;
+	width: 0px;
+	h"
+ +"eight: 42px;
+	background: rgba(255,255,255,0.3);
+	"
+ +"border-radius: 5px;
+	transition: all 2s ease;
+} 
+b"
+ +"utton:hover:before {
+	width: 100%;
+}
+button:hover "
+ +"{
+	background: rgba(0,0,0,0);
+	color: #3a7900;
+	bo"
+ +"x-shadow: inset 0 0 0 3px #3a7999;
+}
+button{ 
+	pad"
+ +"ding: 10px 35px;  
+	overflow:hidden;
+}
+button:befo"
+ +"re {
+	font-family: FontAwesome;
+	content:'\f07a';
+"
+ +"	position: absolute;
+	top: 11px;
+	left: -30px;
+	tr"
+ +"ansition: all 200ms ease;
+}
+button:hover:before {
+"
+ +"	left: 7px;
+}
+.text{
+  margin: 30px 0px 30px 0px;	"
+ +"
+  padding: 1px;
+  background: rgba(0,0,0,0.5);
+  "
+ +"display: inline-block;
+}
+</style>
+  </head>
+<body>"
+ +"
+<div class='portada'>
+		<div class='text'>
+<!----"
+ +"---------CAJA DE CONTENIDOS---------------------->"
+ +"
+<!-------------FIN DE LA CAJA DE CONTENIDOS------"
+ +"---------------------->
+		</div>
+</div>
+
+
+
+  </bod"
+ +"y>
+  </html>
+
+
+");
+// End Hiding -->
+</SCRIPT>
+<!-- Size: 2545 bytes --> 
